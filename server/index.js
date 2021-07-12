6d3a493c79fc35675e6c85c4c2fe0a49a1247310
Alberto Cabrera <57309472+AlbertoCabreraJr@users.noreply.github.com>
2021-07-12 14:33:03 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,8 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://room-app.netlify.app",
-    // origin: "http://localhost:3000",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "DELETE"],
   },
 });
